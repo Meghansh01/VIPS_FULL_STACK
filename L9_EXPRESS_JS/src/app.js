2ad441cb147cef2807087e0a9001d4625ef74d9f
Meghansh <meghansh@example.com>
2025-07-22 09:47:36 +0530

    Express js addons

--- L9_EXPRESS_JS/src/app.js
+++ L9_EXPRESS_JS/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended : true})) /* Makes the data readable unlike req.body*/
 app.get("/",(req, res)=>{
     // res.send("Get route")//
     let {name , email} = req.query
@@ -8,6 +10,16 @@ app.get("/",(req, res)=>{
     res.send("Data aa Gaya")
 }) 
 app.post("/",(req, res)=>{
+     let {name , email} = req.body /*data isn't readable in req.body */
+    console.log("name:",name);
+    console.log("email:",email);
     res.send("Post route")
+})
+/*Dynamic Routing*/
+app.get("/user/:id", (req, res)=>{
+    let{id}= req.params
+
+    console.log("id:",id);
+    res.send("ID received")
 })
 module.exports = app
